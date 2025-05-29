dc77ac7b7171b49babd268def999a9b0a3d1e9ac
Zofia Nora <sophienora92@gmail.com>
2025-05-29 12:36:00 +0200

    added useState to component

--- frontend/src/components/SortButton/Sortbutton.jsx
+++ frontend/src/components/SortButton/Sortbutton.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
 import styles from './SortButton.module.css';
 
-function SortButton() {
+function SortButton({onSortChange}) {
+    const [sortBy, setSortBy] = useState('title');
+    const [sortOrder, setSortOrder] = useState('asc');
+
+    const handleSortChange = (field) => {
+        setSortBy(field);
+        onSortChange(field, sortOrder);
+    };
+
+    const handleOrderChange = (order) => {
+        setSortOrder(order);
+        onSortChange(sortBy, order);
+    };
+
+
   return (
     <>
         <div className={styles.container}> 
