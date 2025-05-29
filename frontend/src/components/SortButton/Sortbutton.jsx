5bfcd1d3f51f2106ccb0ef1d1d88f2049809b41a
Zofia Nora <sophienora92@gmail.com>
2025-05-29 12:26:48 +0200

    added style to button

--- frontend/src/components/SortButton/Sortbutton.jsx
+++ frontend/src/components/SortButton/Sortbutton.jsx
@@ -1,11 +1,15 @@
-
+import styles from './SortButton.module.css';
 
 function SortButton() {
   return (
-    <button className="sort-button">
-        Sort by
-      
-    </button>
+    <>
+        <div className={styles.container}> 
+            <button className={styles.sort_button}>
+                Sort by
+            
+            </button>
+        </div>
+    </>
   );
 }
 
