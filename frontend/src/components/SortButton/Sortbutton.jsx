6d38d89b279b04ced333df8339d89512d48de876
Zofia Nora <sophienora92@gmail.com>
2025-05-29 13:17:46 +0200

    added state

--- frontend/src/components/SortButton/Sortbutton.jsx
+++ frontend/src/components/SortButton/Sortbutton.jsx
@@ -2,23 +2,27 @@ import { useState } from 'react';
 import styles from './SortButton.module.css';
 
 function SortButton() {
+    const [sortBy, setSortBy] = useState('title');
+    const [sortOrder, setSortOrder] = useState('asc');
 
+    const handleSort = (field, order) => {
+        setSortBy(field);
+        setSortOrder(order);
+        onSortChange(field, order); // call parent with new values
+    };
 
   return (
     <>
         <div className={styles.container}> 
-            <button className={styles.sort_button}>
-                Sort by
-            
-            </button>
+
+            <button className={styles.sort_button}>Sort by</button>
+
             <div className={styles.sortOptions}>
                 <button className={styles.toggleButton}>Title</button>
                 <button className={styles.toggleButton}>Date</button>
+                <p className={styles.toggleBorder}></p>
                 <button className={styles.toggleButton}>Asc</button>
                 <button className={styles.toggleButton}>Desc</button>
-
-
-                
             </div>
         </div>
     </>
