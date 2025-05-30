22233541638b25f5cf3fce115d72d348ef169fcd
Izunnaya <ugorjimicheal.um@gmail.com>
2025-05-30 16:04:21 +0100

    Search bar UI design done

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,30 @@
-import styles from './SearchBar.module.css';
+// import styles from "./SearchBar.module.css";
+import { Search } from "lucide-react";
 
 function SearchBar() {
-
   return (
-        <div className= {styles.input_container}>
-            <input
-                type="text"
-                placeholder="Search..."
-                className={styles.search_bar}
-            />
+    <div className="max-w-screen">
+      <div className="relative mx-auto w-max flex justify-between gap-x-5 items-center">
+        <div className=" flex items-center">
+          <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
+            <Search className=" left-5 top-5 h-5 w-5 ml-3 text-gray-400" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="outline-2 lg:w-xl outline-cyan-400 rounded-2xl py-2 px-4 ml-5 pl-8"
+          />
         </div>
+
+        <select className="border rounded p-2 text-black bg-white my-2.5 outline-0">
+          <option value="title">Title</option>
+          <option value="author">Author</option>
+        </select>
+        <div className="px-3 outline-amber-50 bg-amber-50 text-black rounded-lg py-2">
+          Search
+        </div>
+      </div>
+    </div>
   );
 }
 
