6285a67f4d97d5117ae64385c1758a54908b9f57
Izunnaya <ugorjimicheal.um@gmail.com>
2025-06-02 23:47:21 +0100

    App  css conflict resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/footer";
-import ResourceList from "./components/Resources/ResourceList";
-import SearchBar from "./components/SearchBar/SearchBar.jsx";
+
 import SearchPage from "./components/SearchBar/SearchPage";
 
 function App() {
